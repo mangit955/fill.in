0cfe5251e9600b46395b3e5869e1aa57a3fc8244
Manas Raghuwanshi <manasr955@gmail.com>
2026-01-17 11:57:38 +0530

    feat: added popover & scrollarea for block buttons

--- components/builder/AddBlockPanel.tsx
+++ components/builder/AddBlockPanel.tsx
@@ -1,9 +1,9 @@
 "use client";
-
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion,  } from "framer-motion";
 import { BlockButton } from "../ui/blockButton";
-import { CircleCheckBig, Rows2, Rows3 } from "lucide-react";
+import { CircleCheckBig, LayoutDashboard, Rows2, Rows3 } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { ScrollArea } from "../ui/scroll-area";
 
 type Props = {
   onAddShortText: () => void;
@@ -16,14 +16,14 @@ export default function AddBlockPanel({
   onAddShortText,
   onAddMultipleChoice,
 }: Props) {
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="rounded-md p-6 space-y-2">
       {/* Toggle & rotation animation*/}
-      <motion.button
-        onClick={() => setIsOpen((prev) => !prev)}
-        className="text-lg text-gray-300 font-medium  flex items-center gap-2"
+      <Popover>
+        <PopoverTrigger asChild>
+          <motion.button
+        className="text-lg text-gray-400 font-medium  flex items-center gap-2"
       >
         <motion.span
           whileHover={{ rotate: 90 }}
@@ -32,19 +32,20 @@ export default function AddBlockPanel({
         >
           +
         </motion.span>
-        <span>Add a block/</span>
-      </motion.button>
-
-      {/* Animated list */}
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
+        <span>Press to start from scratch</span>
+          </motion.button>
+       </PopoverTrigger>
+       
+      <PopoverContent className="w-fit p-2">
+          <ScrollArea className="h-24 pr-2">
+            <motion.div
             initial={{ height: 0, opacity: 0 }}
             animate={{ height: "auto", opacity: 1 }}
             exit={{ height: 0, opacity: 0 }}
             transition={{ duration: 0.25, ease: "easeOut" }}
-            className="overflow-hidden w-fit"
+            className="overflow-hidden "
           >
+            
             <div className=" rounded-md">
               <BlockButton onClick={onAddShortText}>
                 <Rows2 />
@@ -58,10 +59,28 @@ export default function AddBlockPanel({
                 <CircleCheckBig />
                 Multiple choice
               </BlockButton>
+              <BlockButton onClick={onAddShortText}>
+                <Rows2 />
+                Short text
+              </BlockButton>
+              <BlockButton onClick={onAddLongText}>
+                <Rows3 />
+                Long text
+              </BlockButton>
+              <BlockButton onClick={onAddMultipleChoice}>
+                <CircleCheckBig />
+                Multiple choice
+              </BlockButton>
             </div>
+            
           </motion.div>
-        )}
-      </AnimatePresence>
+          </ScrollArea>
+        </PopoverContent>
+        </Popover>
+      <div className="w-fit items-center flex gap-2 cursor-pointer text-neutral-400 rounded-sm hover:text-neutral-600 hover:bg-gray-100 p-1">
+        <LayoutDashboard size={18} />
+        <span className="font-semibold">Use this templet</span>
+      </div>  
     </div>
   );
 }
